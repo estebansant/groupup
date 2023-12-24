483d22707a3035c92d7129456f3dc7fa245d3954
estebansant <estebansant30@gmail.com>
2023-12-24 12:47:54 -0400

    setting up clerk webhook for user actions

--- lib/mongodb/database/index.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const MONGODB_URI = process.env.MONGODB_URI
-
-let cached = (global as any).mongoose || { conn: null, promise: null };
-
-export const connectToDatabase = async () => {
-    if (cached.conn) return cached.connect;
-}
