add714f1f72a117b84b0975947d5eb6960e3e39e
estebansant <estebansant30@gmail.com>
2023-12-31 21:49:00 -0400

    added cards to homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,8 +3,17 @@ import Image from "next/image"
 import Link from "next/link";
 import React from "react";
 import heroImage from "./../../public/assets/images/hero.png";
+import { Collection } from "./../../components/shared/Collection"
+import { getAllEvents } from "@/lib/actions/event.actions";
 
-export default function Home() {
+export default async function Home() {
+  const events = await getAllEvents({
+    query: "",
+    category: "",
+    page: 1,
+    limit: 6
+  });
+
   return (
     <React.Fragment>
       <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
@@ -34,6 +43,16 @@ export default function Home() {
           Search
           CategoryFilter
         </div>
+
+        <Collection 
+          data={events?.data}
+          emptyTitle="No Events found"
+          emptyStateSubtext="Come back later"
+          collectionType="All_Events"
+          limit={6}
+          page={1}
+          totalPages={2}
+        />
       </section>
     </React.Fragment>
   );
